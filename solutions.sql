0beead28e0cae3c1dda4bf4ec454ba714a3c16ce
NusratParvin <nusrat.official@gmail.com>
2024-10-22 19:59:52 +0400

    table data insertion

--- solutions.sql
+++ solutions.sql
@@ -1,2 +1,59 @@
 -- Active: 1729093794512@@127.0.0.1@5432@university_db
+
+
+--Create a "courses" table with the following fields:
+ 
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    credits INT NOT NULL
+);
+ 
+--Create a "students" table with the following fields:
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    frontend_mark INT,
+    backend_mark INT ,
+    status VARCHAR(50)
+);
+
+--Create an "enrollment" table with the following fields:
+CREATE TABLE enrollments (
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INT NOT NULL REFERENCES students(student_id) ON DELETE CASCADE,
+    course_id INT NOT NULL REFERENCES courses(course_id) ON DELETE CASCADE
+);
+
+
+ --Data insertion
+
+ INSERT INTO courses (course_id, course_name, credits)
+VALUES
+(1, 'Next.js', 3),
+(2, 'React.js', 4),
+(3, 'Databases', 3),
+(4, 'Prisma', 3);
+
+select * from  courses;
+
+INSERT INTO students (student_id, student_name, age, email, frontend_mark, backend_mark, status)
+VALUES
+(1, 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+(2, 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+(3, 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+(4, 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+(5, 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+(6, 'Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
 select * from students;
+INSERT INTO enrollments (enrollment_id, student_id, course_id)
+VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 1),
+(4, 3, 2);
+
+select * from enrollments;
