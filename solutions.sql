b27fa292708ed78c54827307030e2e68d07acbf2
NusratParvin <nusrat.official@gmail.com>
2024-10-22 21:54:41 +0400

    all queries

--- solutions.sql
+++ solutions.sql
@@ -15,10 +15,13 @@ CREATE TABLE students (
     student_name VARCHAR(255) NOT NULL,
     age INT NOT NULL,
     email VARCHAR(100) NOT NULL,
-    frontend_mark INT,
-    backend_mark INT ,
-    status VARCHAR(50)
+    frontend_mark INT NOT NULL,
+    backend_mark INT NOT NULL,
+    status VARCHAR(50) 
 );
+
+-- drop table students,enrollments;
+-- drop table courses;
 
 --Create an "enrollment" table with the following fields:
 CREATE TABLE enrollments (
@@ -29,31 +32,109 @@ CREATE TABLE enrollments (
 
 
  --Data insertion
+ 
 
- INSERT INTO courses (course_id, course_name, credits)
+ INSERT INTO courses ( course_name, credits)
 VALUES
-(1, 'Next.js', 3),
-(2, 'React.js', 4),
-(3, 'Databases', 3),
-(4, 'Prisma', 3);
+('Next.js', 3),
+('React.js', 4),
+('Databases', 3),
+('Prisma', 3);
 
 select * from  courses;
 
-INSERT INTO students (student_id, student_name, age, email, frontend_mark, backend_mark, status)
+INSERT INTO students ( student_name, age, email, frontend_mark, backend_mark, status)
 VALUES
-(1, 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
-(2, 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
-(3, 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
-(4, 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
-(5, 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
-(6, 'Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+('Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+('Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+('Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+('Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+('Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+('Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
 
 select * from students;
-INSERT INTO enrollments (enrollment_id, student_id, course_id)
+INSERT INTO enrollments ( student_id, course_id)
 VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 2, 1),
-(4, 3, 2);
+(1, 1),
+(1, 2),
+(2, 1),
+(3, 2);
 
 select * from enrollments;
+
+
+--Query 1
+-- Insert a new student record 
+
+INSERT INTO students (student_name, age, email, frontend_mark, backend_mark, status)
+VALUES ('Nusrat', 34, 'nusrat@mail.com', 77, 77, NULL);
+
+
+select * from students
+
+
+
+--Query 2
+-- Retrieve the names of all students who are enrolled in the course titled 'Next.js'.
+
+SELECT student_name  FROM students s 
+JOIN enrollments e ON s.student_id = e.student_id 
+JOIN courses c ON e.course_id = c.course_id where course_name='Next.js';
+
+
+--Query 3
+--- Update the status of the student with the highest total (frontend_mark + backend_mark) to 'Awarded'.
+
+UPDATE students
+SET status = 'Awarded'
+WHERE student_id = (
+    SELECT student_id
+    FROM students
+    ORDER BY (frontend_mark + backend_mark) DESC
+    LIMIT 1
+);
+
+
+ -- Query 4
+ --Delete all courses that have no students enrolled.
+
+-- select c.course_id from courses c left join enrollments e ON c.course_id=e.course_id where e.course_id IS NULL;
+
+DELETE FROM courses
+WHERE course_id IN (
+    SELECT c.course_id
+    FROM courses c
+    LEFT JOIN enrollments e ON c.course_id = e.course_id
+    WHERE e.course_id IS NULL
+);
+
+select * from courses
+
+
+-- Query 5 
+--Retrieve the names of students using a limit of 2, starting from the 3rd student.
+
+select student_name from students LIMIT 2 OFFSET 2;
+
+
+--Query 6:
+--Retrieve the course names and the number of students enrolled in each course.
+
+select course_name, count(*) from students s 
+JOIN enrollments e ON s.student_id= e.student_id
+JOIN courses c ON e.course_id=c.course_id
+ GROUP BY course_name
+
+ 
+
+ -- Query 7
+-- Calculate and display the average age of all students.
+
+select ROUND( AVG(age),2) as avg_age from students 
+
+ 
+
+ --Query 8
+ -- Retrieve the names of students whose email addresses contain 'example.com'.
+
+ select student_name from students where email LIKE '%@example.com'
